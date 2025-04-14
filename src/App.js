43c081abac1dd1ed3a2441b418c8ed6cbedf4d6a
rayakhader <rayakhader20@gmail.com>
2025-04-14 12:09:58 +0300

    Initialize app and add Searchbar component to App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Searchbar from './Searchbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Add member to Front-End development team </h1>
+      <Searchbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Searchbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Searchbar() {
+  return (
+    <div>
+        <input type="text" placeholder='Find Members' />
+    </div>
+  )
+}
+
+export default Searchbar
